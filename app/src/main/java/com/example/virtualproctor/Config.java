f051cacabad345c0b8617bd1bc3e63afc8dfe9af
murtaza98 <m.patrawala@somaiya.edu>
2019-03-30 15:40:13 +0530

    integration with backend

--- app/src/main/java/com/example/virtualproctor/Config.java
+++ app/src/main/java/com/example/virtualproctor/Config.java
@@ -3,5 +3,5 @@ package com.example.virtualproctor;
 public class Config {
 //   Harsh : https://ac141924.ngrok.io/
 //   Ojas : https://dc9ee9ee.ngrok.io/
-    final static String URL = "https://dc9ee9ee.ngrok.io/";
+    final static String URL = "http://6e8193e8.ngrok.io/";
 }
