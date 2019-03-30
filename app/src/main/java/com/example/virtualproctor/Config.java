6410db9fc0a45ded838bf0dc6a2be41820195f6c
ojasskapre <ojas.kapre@somaiya.edu>
2019-03-30 13:40:37 +0530

    user login main activity

---
+++ app/src/main/java/com/example/virtualproctor/Config.java
@@ -0,0 +1,7 @@
+package com.example.virtualproctor;
+
+public class Config {
+//   Harsh : https://ac141924.ngrok.io/
+//   Ojas : https://dc9ee9ee.ngrok.io/
+    final static String URL = "https://dc9ee9ee.ngrok.io/";
+}
