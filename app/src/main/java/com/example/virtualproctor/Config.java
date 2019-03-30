a77b15dcd98ab806d551a2dd69aecb54d6f1a3a0
ojasskapre <ojas.kapre@somaiya.edu>
2019-03-31 00:23:32 +0530

    bug fix in student id activity

--- app/src/main/java/com/example/virtualproctor/Config.java
+++ app/src/main/java/com/example/virtualproctor/Config.java
@@ -2,7 +2,7 @@ package com.example.virtualproctor;
 
 public class Config {
 //   Harsh : https://56e7385b.ngrok.io/
-//   Ojas :  https://acdce5bc.ngrok.io/
+//   Ojas :    https://6e771a72.ngrok.io/
 //   Tanay : https://6e8193e8.ngrok.io/
-    final static String URL = "https://6e8193e8.ngrok.io/";
+    final static String URL = "  https://6e771a72.ngrok.io/";
 }
