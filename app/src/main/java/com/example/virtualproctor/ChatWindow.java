006b4e896eaf9f0316eed80edc80de0cc5f0802b
ojasskapre <ojas.kapre@somaiya.edu>
2019-03-31 07:22:48 +0530

    modified template

--- app/src/main/java/com/example/virtualproctor/ChatWindow.java
+++ app/src/main/java/com/example/virtualproctor/ChatWindow.java
@@ -2,6 +2,7 @@ package com.example.virtualproctor;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
